50e3f170df9dbd8bf7c628bce751ef5f82c77f58
Andrew Ganzha <jackbrikman@mail.ru>
2025-12-30 16:51:42 +0300

    Add eruda for prod

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import App from './App.vue'
 import router from './router'
 import './assets/theme.css'
 
-// Load eruda in development to inspect the app on mobile devices.
-if (import.meta.env.DEV) {
-  import('eruda').then(({ default: eruda }) => eruda.init())
-}
+// Always load eruda to inspect the app (useful on mobile and production).
+import('eruda').then(({ default: eruda }) => eruda.init())
 
 const app = createApp(App)
 
